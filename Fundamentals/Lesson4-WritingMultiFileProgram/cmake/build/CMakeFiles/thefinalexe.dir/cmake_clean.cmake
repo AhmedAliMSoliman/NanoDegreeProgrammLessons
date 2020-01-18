17eb01a97e99c0f037e315ea481c8fec9a0a5830
root <root@AhmedAliMSoliman530S.localdomain>
2020-01-18 18:15:55 +0400

    add fund

---
+++ Fundamentals/Lesson4-WritingMultiFileProgram/cmake/build/CMakeFiles/thefinalexe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/thefinalexe.dir/src/increment_and_sum.cpp.o"
+  "CMakeFiles/thefinalexe.dir/src/main.cpp.o"
+  "CMakeFiles/thefinalexe.dir/src/vect_add_one.cpp.o"
+  "thefinalexe"
+  "thefinalexe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thefinalexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
