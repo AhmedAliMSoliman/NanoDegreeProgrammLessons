10cdeec8321f32a79a5b54a8d5e792762780c71e
root <root@AhmedAliMSoliman530S.localdomain>
2020-01-19 20:30:08 +0400

    inhertence

---
+++ NanoDegreeProgrammLessons/OOP/Lesson3-Advance-OOP/3-5.cpp
@@ -0,0 +1,20 @@
+// Inheritance
+// In this exercise you will practice building an inheritance hierarchy.
+
+// Instructions
+// Define a class Animal.
+// Define 3 member variables: color, name, age.
+// Define a derived class Snake that inherits from the base class Animal.
+// Create a member variable length for the Snake class.
+// Create a derived class Cat that inherits from the base class Animal.
+// Create a member variable height for the Cat class.
+// Create MakeSound() member functions for each of the derived classes.
+// In the main() function instantiate Snake and Cat objects. Initialize both their unique and derived attributes. Program them to make a sound.
+
+// Define base class Animal
+
+// Declare derived class Snake
+
+// Declare derived class Cat
+
+// Test in main()
