fad9d0ac323cc58f95ee13827796236dd263a485
Ahmed Ali M. Soliman <ahmed-soliman@hcl.com>
2020-02-22 01:24:14 +0400

    rule of three

--- 3-MemoryMangment/Lesson5-ResourcCopingPolicies/MoveSemantics/rule_of_three.cpp
+++ 3-MemoryMangment/Lesson5-ResourcCopingPolicies/MoveSemantics/rule_of_three.cpp
@@ -20,4 +20,33 @@ public:
         std::cout << "DELETING instance of MyMovableClass at " << this << std::endl;
         delete[] _data;
     }
+    
+    MyMovableClass(const MyMovableClass &source) // 2 : copy constructor
+    {
+        _size = source._size;
+        _data = new int[_size];
+        *_data = *source._data;
+        std::cout << "COPYING content of instance " << &source << " to instance " << this << std::endl;
+    }
+    
+    MyMovableClass &operator=(const MyMovableClass &source) // 3 : copy assignment operator
+    {
+        std::cout << "ASSIGNING content of instance " << &source << " to instance " << this << std::endl;
+        if (this == &source)
+            return *this;
+        delete[] _data;
+        _data = new int[source._size];
+        *_data = *source._data;
+        _size = source._size;
+        return *this;
+    }
 };
+
+int main()
+{
+    MyMovableClass obj1(10); // regular constructor
+    MyMovableClass obj2(obj1); // copy constructor
+    obj2 = obj1; // copy assignment operator
+
+    return 0;
+}
