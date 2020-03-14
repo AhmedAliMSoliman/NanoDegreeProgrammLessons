6c54e5e6bbbb940729c210c180542a8db6761c15
Ahmed Ali M. Soliman <ahmed-soliman@hcl.com>
2020-03-14 23:01:30 +0400

    running single thread again 3

--- 4-Concurrency/Lesson1-IntroductionAndRunningThreads/3.RunningSingleThread/example_1.cpp
+++ 4-Concurrency/Lesson1-IntroductionAndRunningThreads/3.RunningSingleThread/example_1.cpp
@@ -6,8 +6,14 @@ void func()
     std::cout << "Hello concurrent world from main! Thread id = " << std::this_thread::get_id() << std::endl;
 }
 
+void func2()
+{
+    std::cout << std::this_thread::get_id() << std::endl;
+}
+
 int main()
 {
     std::cout << "Hello concurrent world from main! Thread id = " << std::this_thread::get_id() << std::endl;
     func();
+    func2();
 }
